c6544b765a2231e468a450b6e732d04edbb3297b
t-sin <shinichi.tanaka45@gmail.com>
2021-02-07 17:09:52 +0900

    Add entry point

--- main.lisp
+++ main.lisp
@@ -1,7 +1,8 @@
 (defpackage #:scheduler-prototype
   (:use #:cl)
+  (:nicknames #:sp)
   (:export #:start))
 (in-package #:scheduler-prototype)
 
-(defun start ()
-  )
+(defun start (pathname)
+  (scheduler-prototype.event:load-events pathname))
