f867f0beede57a00353039300b519c3dacc032c7
t-sin <shinichi.tanaka45@gmail.com>
2021-02-07 18:13:50 +0900

    Make sound

--- main.lisp
+++ main.lisp
@@ -1,8 +1,19 @@
 (defpackage #:scheduler-prototype
   (:use #:cl)
   (:nicknames #:sp)
-  (:export #:start))
+  (:import-from #:scheduler-prototype.event
+                #:load-events)
+  (:export #:start
+           #:stop))
 (in-package #:scheduler-prototype)
 
-(defun start (pathname)
-  (scheduler-prototype.event:load-events pathname))
+(defstruct transport
+  (bpm 120) )
+
+(defun start (&optional (pathname "./sequence-data.lisp"))
+  (load-events pathname)
+  (sp-sound:init)
+  (sp-sound:start #'sp-sound:process-signal))
+
+(defun stop ()
+  (sp-sound:stop))
